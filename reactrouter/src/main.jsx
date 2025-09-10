e53bf286f73f01fa418b442eae71f783ff86e1ac
akghosh111 <ghoshkiran2017@gmail.com>
2025-09-10 20:38:15 +0530

    added navlinks and link, active state

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 
 const router = createBrowserRouter([
   {
