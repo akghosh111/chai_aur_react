b923078d5e270465ec22be0f9887411f3586e1f0
akghosh111 <ghoshkiran2017@gmail.com>
2025-09-10 23:08:16 +0530

    added github info loader

--- reactrouter/src/components/Github/Github.jsx
+++ reactrouter/src/components/Github/Github.jsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch("https://api.github.com/users/akghosh111")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, [])
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch("https://api.github.com/users/akghosh111")
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
 
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
       Github followers: {data.followers}
+
+      <img className='' src={data.avatar_url} alt="Github profile picture" width={300} />
     </div>
   )
 }
 
 export default Github
+
+
+export const githubInfoLoader = async () => {
+    const response = await fetch("https://api.github.com/users/akghosh111")
+    return response.json()
+}
