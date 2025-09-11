5105614f99110083535dfb9cb776ef4307d57ae0
akghosh111 <ghoshkiran2017@gmail.com>
2025-09-11 17:44:33 +0530

    adding login component with context

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -1,11 +1,12 @@
+import UserContextProvider from "./context/UserContextProvider"
 
 function App() {
   
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Chai</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
